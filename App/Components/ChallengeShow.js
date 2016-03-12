4a87addbb9ef7bf005a4a70c4b58728a0879e327
alanahanson <alanahanson@gmail.com>
2016-03-12 13:21:09 -0600

    update challengePage

--- App/Components/ChallengeShow.js
+++ App/Components/ChallengeShow.js
@@ -1,109 +1,28 @@
-'use strict';
-import React, {
-  AppRegistry,
-  Component,
+var React = require('react-native');
+
+var {
+  View,
+  Text,
   Image,
   ListView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
-var fake_challenge_data = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://listsurge.com/wp-content/uploads/2015/08/t-hill-s-top-movies-of-2011-so-far-.jpg'}},
-];
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
-
-class AwesomeProject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  }
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    // var movie = this.state.movies[0];
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={styles.ListView}/>
-      );
-  }
-
-    renderLoadingView() {
-      return (
-        <View style={styles.container}>
-          <Text>
-            Loading movies...
-          </Text>
-        </View>
-      );
-    }
-
-    renderMovie(movie) {
-      return (
-        <View style={styles.container}>
-          <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{movie.title}</Text>
-            <Text style={styles.year}>{movie.year}</Text>
-          </View>
-        </View>
-        );
-    }
-  }
+  StyleSheet
+} = React;
 
 var styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#111111'
   },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+class ChallengePage extends React.Component
+  constructor
+  render({
+    return (
+      <View>
+        <Text>Challenge Name</Text>
+      </View>
+      )
+  }
+
+  module.exports = ChallengePage;
