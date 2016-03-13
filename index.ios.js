962563c1c2d69948bab477fb84c21909b8306827
Michal Klimek <mklimek15@gmail.com>
2016-03-12 18:51:23 -0600

    create working routes from user page to groups to members/challenges/comments

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,10 @@
 import React from 'react-native';
 // var NativeImagePicker = require('./App/Components/NativeImagePicker');
 var User = require('./App/Components/User');
+var GroupPage = require('./App/Components/GroupPage');
+var GroupMembers = require('./App/Components/GroupMembers');
+var GroupChallenges = require('./App/Components/GroupChallenges');
+var GroupComments = require('./App/Components/GroupComments');
 // var ChallengeShow = require('./App/Components/ChallengeShow');
 // var GroupChallenges = require('./App/Components/GroupChallenges');
 
@@ -21,27 +25,29 @@ const {
 } = React;
 
 class GrouVieReact extends React.Component {
+  renderScene(route, navigator) {
+    if (route.component == User) {
+      return <User navigator={navigator} {...route.passProps} />
+    }
+    if (route.component == GroupPage) {
+      return <GroupPage navigator={navigator} {...route.passProps} />
+    }
+    if (route.component == GroupMembers) {
+      return <GroupMembers navigator={navigator} {...route.passProps} />
+    }
+    if (route.component == GroupChallenges) {
+      return <GroupChallenges navigator={navigator} {...route.passProps} />
+    }
+    if (route.component == GroupComments) {
+      return <GroupComments navigator={navigator} {...route.passProps} />
+    }
+  }
+
   render() {
     return (
-      <Navigator
-        initialRoute={{name: 'My First Scene', index: 0}}
-        renderScene={(route, navigator) =>
-          <User
-            name={route.name}
-            onForward={() => {
-              var nextIndex = route.index + 1;
-              navigator.push({
-                name: 'Scene ' + nextIndex,
-                index: nextIndex,
-              });
-            }}
-            onBack={() => {
-              if (route.index > 0) {
-                navigator.pop();
-              }
-          }}/>
-      } />
-    );
+      <Navigator style={{flex:1}} initialRoute={{component: User}} renderScene={this.renderScene} />
+
+    )
   }
 };
 
@@ -53,5 +59,24 @@ var styles = StyleSheet.create({
   },
 });
 
+
+      //   initialRoute={{name: 'My First Scene', index: 0}}
+      //   renderScene={(route, navigator) =>
+      //     <User
+      //       navigator={navigator}
+      //       name={route.name}
+      //       onForward={() => {
+      //         var nextIndex = route.index + 1;
+      //         navigator.push({
+      //           name: 'Scene ' + nextIndex,
+      //           index: nextIndex,
+      //         });
+      //       }}
+      //       onBack={() => {
+      //         if (route.index > 0) {
+      //           navigator.pop();
+      //         }
+      //     }}/>
+      // } />
 
 React.AppRegistry.registerComponent('GrouVieReact', () => GrouVieReact);
