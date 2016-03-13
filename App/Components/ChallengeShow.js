ca5e26241116d5a3708115ac04a8bd468999b5fd
Marshall Sosland <email": null>
2016-03-13 15:11:42 -0500

    begin camera integration

--- App/Components/ChallengeShow.js
+++ App/Components/ChallengeShow.js
@@ -1,39 +1,97 @@
 var React = require('react-native');
+var NativeImagePicker = require('./NativeImagePicker');
 
 var {
   View,
+  NativeImagePicker,
   Component,
   Text,
   Image,
   ListView,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  TouchableOpacity,
+  NativeModules: {
+    ImagePickerManager
+  }
 } = React;
 
-var styles = StyleSheet.create({
-  container:{
-    top: 40,
+const styles = StyleSheet.create({
+  container: {
+    top: 50,
     flex: 1,
-    // backgroundColor: '#111111'
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
+  avatarContainer: {
+    borderColor: '#9B9B9B',
+    borderWidth: 1 / PixelRatio.get(),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    borderRadius: 0,
+    width: 150,
+    height: 150
+  }
 });
 
 class ChallengeShow extends Component {
   constructor(props) {
     super(props);
-    console.log("print");
-    console.log(this.props.challenge.participations);
     this.state = {
-      username: '',
-      image_url: ''
+      avatarSource: null
     };
   }
+
+  selectPhotoTapped() {
+    const options = {
+      title: 'Photo Picker',
+      quality: 0.5,
+      maxWidth: 300,
+      maxHeight: 300,
+      storageOptions: {
+        skipBackup: true
+      }
+    };
+    ImagePickerManager.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+      console.log(response.uri);
+
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+      }
+      else if (response.error) {
+        console.log('ImagePickerManager Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        // You can display the image using either:
+        //const source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+        const source = {uri: response.uri.replace('file://', ''), isStatic: true};
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
+
   render(){
     var participations = this.props.challenge.participations;
     var list = participations.map((item, index) => {
       return (
         <View key={index}>
-          <View><Text>{participations[index].username}</Text><Image source="{participations[index].image_url}" /></View></View>
+          <View><Text>{participations[index].username}</Text><TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+          <View style={[styles.avatar, styles.avatarContainer, {marginBottom: 20}]}>
+          { this.state.avatarSource === null ? <Text>Select a Photo</Text> :
+            <Image style={styles.avatar} source={this.state.avatarSource} />
+          }
+          </View>
+        </TouchableOpacity></View></View>
           )
                 // Something to press to trigger CameraRoll
                 //       <TouchableHighlight
